e716b8302b2599f0c2286fd648d52ddd51895f7f
unknown <a7medalqalawyi@gmail.com>
2024-07-04 03:09:45 +0300

    Second

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1540137312"
+    serviceWorkerVersion: "3403163296"
   }
 });
