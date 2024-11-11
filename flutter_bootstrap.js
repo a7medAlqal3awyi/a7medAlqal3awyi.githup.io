243d3bd21e66c983c0f293fb92eb35438281d90b
Ahmed Khaled <a7medalqalawyi@gmail.com>
2024-11-11 16:41:56 +0200

    Update presentation image

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3817909955"
+    serviceWorkerVersion: "883903630"
   }
 });
